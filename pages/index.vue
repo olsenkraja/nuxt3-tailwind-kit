71d38b44e83b8ddaedb6711378cb60bffccac91a
Olsen Kraja <krajaolsen@gmail.com>
2023-02-15 00:57:20 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'landing',
+})
+
+useHead({
+  title: 'Home',
+})
+</script>
+
+<template>
+  <div>
+    <HomeFeatureTwoColumn />
+
+    <HomeFeatureGrid />
+
+    <HomeCta />
+  </div>
+</template>
